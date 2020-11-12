afbfe822bb84cd0239d98479b9b0b6962846d9c1
nusratarif <nusratarif02@gmail.com>
2020-11-12 08:13:35 +0500

    added build

---
+++ build/precache-manifest.fd92877fe2993a847eb42b14673c1064.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e292971a8a51105c33fed5d09241ed16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04b9182f3658492de8c2",
+    "url": "/static/css/main.a343ebc5.chunk.css"
+  },
+  {
+    "revision": "b9a89deac7f4de4f2c7c",
+    "url": "/static/js/2.3e274853.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3e274853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04b9182f3658492de8c2",
+    "url": "/static/js/main.6d9a4e64.chunk.js"
+  },
+  {
+    "revision": "e27b5af4c66cae20f5a3",
+    "url": "/static/js/runtime-main.a42d6432.js"
+  },
+  {
+    "revision": "bca70cfc21fe52e87a78458c04f67420",
+    "url": "/static/media/Background.bca70cfc.jpg"
+  },
+  {
+    "revision": "d130a41a7b6375529f80bfef172ccc6c",
+    "url": "/static/media/BlackNike.d130a41a.jpg"
+  },
+  {
+    "revision": "0047954462211a6f82b83ed6313fc140",
+    "url": "/static/media/BlueSimple.00479544.jpg"
+  },
+  {
+    "revision": "0b56bf9c30d0c6f0764085517aed92fe",
+    "url": "/static/media/CutShoes.0b56bf9c.jpg"
+  },
+  {
+    "revision": "2675febc245c82e6c67bd04289899a3c",
+    "url": "/static/media/GrayShoes.2675febc.jpg"
+  },
+  {
+    "revision": "552053b091bca5c2c57c14b2dc826584",
+    "url": "/static/media/LeatherShoe.552053b0.jpg"
+  },
+  {
+    "revision": "d498cdbab6c15c93ef9fbbc30f1655ae",
+    "url": "/static/media/LoveJoggers.d498cdba.jpg"
+  },
+  {
+    "revision": "c23751483a5d214ebe7f47fdd4aaf740",
+    "url": "/static/media/MenSportsShoes.c2375148.jpg"
+  },
+  {
+    "revision": "1c9e5c818964b5d4c95de94e0c76f280",
+    "url": "/static/media/MenWhiteNikeShoes.1c9e5c81.jpg"
+  },
+  {
+    "revision": "ef754a1a704f9850591a14f480f6f9da",
+    "url": "/static/media/MulticolorNike.ef754a1a.jpg"
+  },
+  {
+    "revision": "81d05a4c7ff23337b969bc38d10f70af",
+    "url": "/static/media/RainbowSports.81d05a4c.jpg"
+  }
+]);
